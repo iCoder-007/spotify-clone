a4d5d39ee3515422ed2a704b49eb61e7c6abc4b3
iCoder-007 <ssygi0007@gmail.com>
2021-02-28 06:19:36 -0500

    spotify-clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react'
 import './App.css';
-
+import Login from './Login'
+import Player from './Player'
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import { useDataLayerValue } from "./DataLayer";
+const spotify=new SpotifyWebApi();
 function App() {
+  const [{user,token},dispatach]= useDataLayerValue()
+  useEffect(() => {
+    const hash=getTokenFromUrl();
+    window.location.hash="";
+const _token=hash.access_token;
+if(_token){
+  dispatach({
+    type:'SET_TOKEN',
+    token:_token
+  })
+  spotify.setAccessToken(_token);
+  spotify.getMe().then(user=>{
+    dispatach({
+      type:'SET_USER',
+      user:user
+    });
+  });
+  spotify.getUserPlaylists().then((playlists)=>{
+    dispatach({
+      type:'SET_PLAYLISTS',
+      playlists:playlists
+    })
+  })
+  spotify.getPlaylist("37i9dQZEVXcIJazRV9ISoM").then((response) =>{
+    dispatach({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+})
+}
+
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        token?(<Player spotify={spotify}/>):(<Login/>)
+      }
+     
     </div>
   );
 }
